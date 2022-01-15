3d177f00183ff458a9ef4838444c0afecfa1c391
KirillTeplov1992 <92922996+KirillTeplov1992@users.noreply.github.com>
2022-01-15 11:46:09 +0700

    Add files via upload

---
+++ moving_tasks/moving_around_and_together.js
@@ -0,0 +1,195 @@
+function onClickButton1() {
+	var ansver = document.getElementById('ansver1');
+	var buttonText = document.getElementById('button1');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "2 часа";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton2() {
+	var ansver = document.getElementById('ansver2');
+	var buttonText = document.getElementById('button2');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "60 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton3() {
+	var ansver = document.getElementById('ansver3');
+	var buttonText = document.getElementById('button3');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "250 км";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton4() {
+	var ansver = document.getElementById('ansver4');
+	var buttonText = document.getElementById('button4');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "60 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton5() {
+	var ansver = document.getElementById('ansver5');
+	var buttonText = document.getElementById('button5');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "5 часов";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton6() {
+	var ansver = document.getElementById('ansver6');
+	var buttonText = document.getElementById('button6');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "42 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton7() {
+	var ansver = document.getElementById('ansver7');
+	var buttonText = document.getElementById('button7');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "204 км";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton8() {
+	var ansver = document.getElementById('ansver8');
+	var buttonText = document.getElementById('button8');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "3 минуты";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton9() {
+	var ansver = document.getElementById('ansver9');
+	var buttonText = document.getElementById('button9');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "24 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton10() {
+	var ansver = document.getElementById('ansver10');
+	var buttonText = document.getElementById('button10');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "30 минут";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton11() {
+	var ansver = document.getElementById('ansver11');
+	var buttonText = document.getElementById('button11');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "57 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton12() {
+	var ansver = document.getElementById('ansver12');
+	var buttonText = document.getElementById('button12');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "120 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton13() {
+	var ansver = document.getElementById('ansver13');
+	var buttonText = document.getElementById('button13');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "540 раз";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton14() {
+	var ansver = document.getElementById('ansver14');
+	var buttonText = document.getElementById('button14');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "90 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+
+function onClickButton16() {
+	var ansver = document.getElementById('ansver16');
+	var buttonText = document.getElementById('button16');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "2 часа";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
---
+++ moving_tasks/moving_tasks.js
@@ -0,0 +1,105 @@
+function onClickButton1() {
+	var ansver = document.getElementById('ansver1');
+	var buttonText = document.getElementById('button1');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "72 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+	
+}
+
+function onClickButton2() {
+	var ansver = document.getElementById('ansver2');
+	var buttonText = document.getElementById('button2');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "48 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+	
+}
+
+function onClickButton3() {
+	var ansver = document.getElementById('ansver3');
+	var buttonText = document.getElementById('button3');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "10 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton4() {
+	var ansver = document.getElementById('ansver4');
+	var buttonText = document.getElementById('button4');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "10 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton5() {
+	var ansver = document.getElementById('ansver5');
+	var buttonText = document.getElementById('button5');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "11 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton6() {
+	var ansver = document.getElementById('ansver6');
+	var buttonText = document.getElementById('button6');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "20 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton7() {
+	var ansver = document.getElementById('ansver7');
+	var buttonText = document.getElementById('button7');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "14 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
+
+function onClickButton8() {
+	var ansver = document.getElementById('ansver8');
+	var buttonText = document.getElementById('button8');
+	if (buttonText.innerHTML == "Показать ответ: ") {
+		ansver.innerHTML = "14 км/ч";
+		buttonText.innerHTML = "Спрятать ответ";
+		}
+	else {
+		 ansver.innerHTML = "";
+		 buttonText.innerHTML = "Показать ответ: ";
+	}
+}
